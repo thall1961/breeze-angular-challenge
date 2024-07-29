2dedebefae43314b6445138699e1b68a78379dd5
Tom Hall <thall1961@gmail.com>
2024-07-29 12:44:12 -0500

    initial commit override

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-app';
+  url = 'https://pokeapi.co/api/v2/pokemon/ditto'
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get(this.url).subscribe(res => console.log('res', res));
+  }
+
 }
